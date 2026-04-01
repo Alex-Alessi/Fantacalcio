3b3734e5cb653d5b30c54c18ef26fa6da6e592af
Alex Alessi <alex.alessi246@gmail.com>
2026-04-01 16:07:14 +0200

    views

--- fantacalcio/game/views.py
+++ fantacalcio/game/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Giornata
+from .models import Giornata, PartitaLega
 from django.utils import timezone
 
 # Create your views here.
@@ -13,9 +13,19 @@ def home(request):
         giornata_iniziata = now >= giornata.orario_inizio
         if not giornata_iniziata:
             tempo_rimanente = (giornata.orario_inizio - now).total_seconds()
+    ultima_giocata = PartitaLega.objects.filter(utente=request.user, giornata__finita=True).order_by('-giornata__orario_inizio').first()
+    prossime = PartitaLega.objects.filter(utente=request.user, giornata__finita=False).order_by('giornata__orario_inizio')
+    if prossime.count() > 1:
+        prossima_partita=prossime[1]
+    elif prossime.count() == 1:
+        prossima_partita = prossime[0]
+    else:
+        prossima_partita = None
     context={
         "giornata":giornata,
         "giornata_iniziata": giornata_iniziata,
         "tempo_rimanente": tempo_rimanente,
+        "ultima_giocata": ultima_giocata,
+        "prossima_partita": prossima_partita,
     }
     return render(request, "game/home.html", context)
